ec4ae0fc3e6cd1bf2b30ad7f7950fd593794fea7
Thiago <thiagodelgado.dev@gmail.com>
2024-09-08 10:29:46 -0300

    delete intentional error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 import * as Sentry from '@sentry/react';
 
 const App = () => {
-  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
   return (
     <main className="bg-black">
       <Navbar />
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
+  dsn: "https://9a22104c7b12422183777e623ef6435e@o4507891618480128.ingest.us.sentry.io/4507891655901184",
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.metrics.metricsAggregatorIntegration(),
@@ -18,14 +18,10 @@ Sentry.init({
       blockAllMedia: false,
     }),
   ],
-  tracesSampleRate: 1.0, 
+  tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0, 
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById("root"));
